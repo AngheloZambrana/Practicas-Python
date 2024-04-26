b8901fc65e2ea4cfc4922b351abe9e4d98cd7027
AngheloZambrana <anghelozambrana2005@gmail.com>
2024-04-26 10:05:17 -0400

    Correccion del ejercicio de invertir Lista

--- Ejercicios/InvertirLista.py
+++ Ejercicios/InvertirLista.py
@@ -1,17 +1,5 @@
 # Definir una función inversa() que calcule la inversión de una cadena.
 # Por ejemplo la cadena "estoy probando" debería devolver la cadena "odnaborp yotse"
-
-def longitudLista():
-    lista = input("Ingrese su lista de datos separados por espacios: ").split()
-    contador = 0
-    for i in lista:
-        contador += 1
-    return contador
-
-contador = longitudLista()
-print("La longitud de la lista es:", contador)
-
-
 def invertirLista(lista):
     cadena_invertida = ""
     for elemento in reversed(lista):

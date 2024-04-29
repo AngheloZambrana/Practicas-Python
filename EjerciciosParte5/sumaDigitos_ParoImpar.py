8cad7430b34edf88781f55ceb381518aeff120c8
AngheloZambrana <anghelozambrana2005@gmail.com>
2024-04-29 14:19:11 -0400

    Correccion de logica en ejercicio de suma de digitos par o impar

--- EjerciciosParte5/sumaDigitos_ParoImpar.py
+++ EjerciciosParte5/sumaDigitos_ParoImpar.py
@@ -2,5 +2,9 @@ def sumaDigitos():
     numero = [int(x) for x in str(int(input("Ingrese su numero: ")))]
     suma = sum(numero)
     print(f"La suma de los dígitos es: {suma}")
+    if suma % 2 == 0:
+        print("La suma de los digitos es par")
+    else:
+        print("La suma es impar")
 
 sumaDigitos()
